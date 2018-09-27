e7d55d60c5350a7bb5d6b9fdfb06129040195008
guojiawei <guojiawei@32gold.com>
2018-09-27 18:59:54 +0800

    通用接口配置修改

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -5,8 +5,7 @@ import qs from 'qs'
 import { Toast, MessageBox} from 'mint-ui'
 import store from '../store'
 import { removeCookie,IP }  from '@/config/mUtils.js'
-// axios.defaults.baseURL = process.env.API_ROOT   //配置接口地址
-axios.defaults.baseURL = "http://192.168.1.179:9191"   //配置接口地址
+axios.defaults.baseURL = process.env.API_ROOT   //配置接口地址
 // axios.defaults.timeout = 5000; //配置请求的超时时间，超时将被中断
 //POST传参序列化(添加请求拦截器)
 axios.interceptors.request.use((config) => {
