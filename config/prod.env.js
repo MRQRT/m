1c222c5d142dd86d6ca7399ba537e254f71390ed
guojiawei <guojiawei@32gold.com>
2018-09-27 15:10:18 +0800

    测试接口地址修改

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT:'"https://openapi.au32.cn"'
+  // API_ROOT:'"https://openapi.au32.cn"'
   // API_ROOT:'"http://10.88.88.227:9191"'
   //  API_ROOT:'"http://101.201.197.238:8099"'
-  // API_ROOT:'"https://openapitest.au32.cn"'
+  API_ROOT:'"https://openapitest.au32.cn"'
 }
