4a9b11b8f7ab77f25bb2347efff0503db9fe2517
guojiawei <guojiawei@32gold.com>
2018-11-12 18:27:29 +0800

    代码哟话

--- src/pages/stor/stor.vue
+++ src/pages/stor/stor.vue
@@ -551,7 +551,6 @@
 				var that = this,
 					uuid = uuidv1(),
 					random = Math.random().toString(36).substr(2);
-
 				let fd = new FormData();
 				fd.append('name',item.name)
 				fd.append('key',this.param_policy.dir+'/'+random+'-'+uuid+'-'+item.name)
@@ -559,10 +558,8 @@
 				fd.append('OSSAccessKeyId',this.param_policy.accessKeyId)
 				fd.append('signature',this.param_policy.signature)
 				fd.append('success_action_status','200')
-
 				fd.append('file',item.file);
 				that.uploadImage(fd,item,uuid,random);
-
 				// var img_size=item.size
 				// if(img_size/1024/1024>3){
 				// 	//进行压缩
@@ -585,78 +582,6 @@
 					duration: 800,
 				});
 			},
-    		// 上传图片
-    		submit () {
-				Indicator.open();
-        		var dataURLToBlob=function(url){
-                	var arr=url.split(','),mime=arr[0].match(/:(.*?);/)[1],
-                	bstr=atob(arr[1]),n=bstr.length,u8arr=new Uint8Array(n);
-                	while(n--){
-                    	u8arr[n]=bstr.charCodeAt(n);
-                	}
-                	return new Blob([u8arr],{type:mime});
-				}
-				//base64转换成二进制文件
-				let formData = new FormData()
-        		this.files.forEach((item, index) => {
-          			var img_size=item.size
-					var img = new Image,
-					canvas = document.createElement("canvas"),
-					ctx = canvas.getContext("2d");
-					img.crossOrigin = "Anonymous";
-					img.src = item.src
-					if(this.AndroVerson>4||this.iosVerson>10){
-						img.onload =() => {
-							var width = img.width;
-							var height = img.height;
-							// 最大上传不得查过500k
-							var rate = (img_size/(1024*500)).toFixed(1)
-							if(rate*1>1){
-								var real_rate = (width<height ? width/height : height/width)/rate;
-								canvas.width = width*real_rate;
-								canvas.height = height*real_rate;
-								ctx.drawImage(img,0,0,width,height,0,0,width*real_rate,height*real_rate);
-								var src1 = canvas.toDataURL("image/jpg");
-								var blob=dataURLToBlob(src1)
-								formData.append('files', blob,'image.jpg')
-							}else{
-								formData.append('files', item.file)
-							}
-							if(index==(this.files.length-1)){ //formdata已创建完
-								xhr_send(this)
-							}
-						}
-					}else{
-						formData.append('files', item.file)
-						if(index==(this.files.length-1)){ //formdata已创建完
-							xhr_send(this)
-						}
-					}
-
-				})
-				function xhr_send(val){
-					// 新建请求
-					const xhr = new XMLHttpRequest()
-					xhr.open('POST', val.url, true)
-					xhr.send(formData)
-					xhr.onload = () => {
-						if (xhr.status === 200 || xhr.status === 304) {
-							let datas = JSON.parse(xhr.responseText)
-							if(datas.code==100){
-								// 存储返回的地址
-								datas.content.forEach((item)=> {
-									val.order.urls.push(item)
-									val.files = [] // 清空文件缓存
-									Indicator.close()
-								})
-							} else {
-								val.$toast('请求错误')
-								Indicator.close()
-							}
-						}
-					}
-				}
-    		},
 		},
 		activated: function () {
 
