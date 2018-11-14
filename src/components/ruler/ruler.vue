54d2c41c5dd09cd23335ed3425b46dff80a371bd
guojiawei <guojiawei@32gold.com>
2018-11-14 17:56:38 +0800

    输入最大刻度修改

--- src/components/ruler/ruler.vue
+++ src/components/ruler/ruler.vue
@@ -44,7 +44,7 @@ export default{
 			var str = clearNoNum(value,2);
 			if(Number(str)>10000){
 				this.gram=10000
-				this.$store.commit('set_rulerData', 5000)
+				this.$store.commit('set_rulerData', '10000')
 			}else if(str==''){
 				this.$store.commit('set_rulerData', '')
 			}else{
